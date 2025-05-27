8b963f090e2c7310346102f5fb7b854b1817a92e
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2025-05-27 21:56:57 +0900

    Finish lesson 306

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,6 +11,16 @@ class ViewController: UIViewController {
         guard let number = Double(displayLabel.text!) else {
             fatalError("Cannot convert display label text to a Double.")
         }
+
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+            } else if calcMethod == "AC" {
+                displayLabel.text = "0"
+            } else if calcMethod == "%" {
+                displayLabel.text = String(number * 0.01)
+            }
+        }
     }
 
     @IBAction func numButtonPressed(_ sender: UIButton) {
